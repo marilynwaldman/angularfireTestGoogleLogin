0515f6146c7e00386aa59bdff914628c8930d7dc
MarilynWaldman <mdwaldman22@hotmail.com>
2018-09-29 14:55:53 -0600

    first push

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDVfIc-KDTAyVlAJWjpqfJsotbvuYekcFY',
+    authDomain: 'ng-fitness-tracker-6483f.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-6483f.firebaseio.com',
+    projectId: 'ng-fitness-tracker-6483f',
+    storageBucket: 'ng-fitness-tracker-6483f.appspot.com',
+    messagingSenderId: '687010565252'
+  }
+};
